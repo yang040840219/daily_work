44a7c9b54d06136a78ad3fd0c94e8e99ef9f4b6c
yxl040840219 <yxl040840219@126.com>
2015-05-20 18:14:50 +0800

    test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -27,8 +27,6 @@ public class TestMain extends AbstractJUnit4SpringContextTests  {
 //    	 System.out.println(value);
     	 value = helloService.getMessage("b");
     	 System.out.println(value);
-
-    	 System.out.println("123");
      log.info("test"); 
     	 
     }
