60dbf3afa92233357406dfdd79b2bff4c42c6c86
yxl040840219 <yxl040840219@126.com>
2015-05-26 12:06:13 +0800

    update

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,11 +1,20 @@
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
 
 import com.cache.service.HelloService;
 
@@ -17,6 +26,15 @@ public class TestMain extends AbstractJUnit4SpringContextTests  {
 	
 	@Autowired
 	private HelloService helloService ;
+	
+	
+	@Resource(name="redisTemplate")
+	private ValueOperations<Serializable,Serializable> valueOperations ;
+	
+	
+	@Autowired
+	private JedisPool jedisPool ;
+	
 	
     @Test
     public void test(){
@@ -31,4 +49,34 @@ public class TestMain extends AbstractJUnit4SpringContextTests  {
     	 
     }
 		
+    @Test
+    public void test1(){
+     Jedis jedis = jedisPool.getResource() ;
+     
+    }
+    
+    
+    public boolean lock3(String key){
+    	Jedis jedis = jedisPool.getResource() ;
+    	  long nano = System.nanoTime();
+          try {
+              while ((System.nanoTime() - nano) < 1000) {
+                  jedis.watch(key);
+                  // 开启watch之后，如果key的值被修改，则事务失败，exec方法返回null
+                  String value = jedis.get(key);
+                  if (value == null || value.equals("UNLOCK")) {
+                      Transaction t = jedis.multi();
+                      t.set(key,"123");
+                      if (t.exec() != null) {
+                          return true;
+                      }
+                  }
+                  jedis.unwatch();
+              }
+          } catch (Exception e) {
+        	     e.printStackTrace();
+          }
+          return false;
+     }
+    
 }
